9ed51197680aae5e3552f077badb8cda8316183b
Atreya Bain <christophervrose@gmail.com>
2021-09-19 10:54:46 +0530

    [add] deploy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withPWA = require('next-pwa');
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
     reactStrictMode: true,
+    // TODO switch to a proper subdomain later
+    basePath:'/bdrec',
     pwa: {
         dest: 'public',
         // disable if not production
